3773bc18d4b6a6d47eed6dcdab7b030fecd6e525
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2023-11-04 15:18:01 +0200

    RabbitMQ

--- src/AuctionService/Data/AuctionDbContext.cs
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -1,6 +1,6 @@
 using AuctionService.Entities;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
-
 namespace AuctionService.Data
 {
     public class AuctionDbContext : DbContext
@@ -9,5 +9,12 @@ namespace AuctionService.Data
         {
         }
         public DbSet<Auction> Auctions { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.AddInboxStateEntity();
+            modelBuilder.AddOutboxMessageEntity();
+            modelBuilder.AddOutboxStateEntity();
+        }
     }
 }
---
+++ src/SearchService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SearchService.Consumers;
+using SearchService.Models;
+using Contracts;
+namespace SearchService;
+
+public class MappingProfiles:Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<AuctionCreated,Item>();
+        CreateMap<AuctionCreated,Item>();
+    }
+}
