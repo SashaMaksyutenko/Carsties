0c96487b322f1f5162e59d8fc6243716502074bc
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2023-12-03 17:33:17 +0200

    CRUD operations in the client app with some issues

--- frontend/web-app/app/session/AuthTest.tsx
+++ frontend/web-app/app/session/AuthTest.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from 'react'
-import { UpdateAuctionTest } from '../actions/auctionActions';
+import { updateAuctionTest } from '../actions/auctionActions';
 import { Button } from 'flowbite-react';
 export default function AuthTest() {
     const[loading,setLoading]=useState(false);
@@ -8,7 +8,7 @@ export default function AuthTest() {
     function doUpdate(){
         setResult(undefined);
         setLoading(true)
-        UpdateAuctionTest()
+        updateAuctionTest()
             .then(res=>setResult(res))
             .finally(()=>setLoading(false))
     }
