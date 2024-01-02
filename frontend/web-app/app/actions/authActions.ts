bfdc18a84db67e8a57587c77be116745b3803863
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2024-01-02 15:48:11 +0200

    issues in app production #2

--- frontend/web-app/app/actions/authActions.ts
+++ frontend/web-app/app/actions/authActions.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from '../api/auth/[...nextauth]/nextauthoptions'
 import { getToken } from "next-auth/jwt";
 import {headers,cookies} from 'next/headers'
 import { NextApiRequest } from "next";
